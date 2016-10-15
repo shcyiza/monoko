f31ff37c0e46f81e48469b284dc1694268fc3f31
Laetitia Massa <laetitiamassa@gmail.com>
2016-10-15 20:18:27 +0200

    fixed gems for heroku, designed new word page

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,6 @@
 class WordsController < ApplicationController
   before_action :set_word, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   # GET /words
   # GET /words.json
