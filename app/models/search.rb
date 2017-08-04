e3a655d04db1a86e46fa649577cefc0a4d5c98fc
Laetitia Massa <laetitiamassa@gmail.com>
2017-08-04 17:49:54 +0100

    made association alphabetic order

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,6 @@
 class Search < ActiveRecord::Base
 	def search_words
-	   words = Word.all 
+	   words = Word.all.order('name') 
 	   words = words.where("lower(name) like lower(?)", "%#{keywords}%") if keywords.present?
 	   return words
 	end
