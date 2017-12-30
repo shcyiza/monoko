8ec360ea087ec7cd407732ef99ee3f991d76a5ac
Laetitia Massa <laetitiamassa@gmail.com>
2017-12-30 13:05:45 +0100

    made search only find identical word typed

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,7 @@
 class Search < ActiveRecord::Base
 	def search_words
 	   words = Word.all.order('name') 
-	   words = words.where("lower(name) like lower(?)", "%#{keywords}%") if keywords.present?
+	   words = words.where("lower(name) like lower(?)", "#{keywords}") if keywords.present?
 	   return words
 	end
 end
