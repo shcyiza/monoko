1a2c32e7f95a86400995f87538403cf3fd1a62cf
shcyiza <patick.cyiza@gmail.com>
2018-04-23 23:36:50 +0200

    integrate db changes done

--- app/models/exemple.rb
+++ app/models/exemple.rb
@@ -1,13 +1,6 @@
 class Exemple < ActiveRecord::Base
 	belongs_to :word
-	belongs_to :definition
-	belongs_to :contributor, :class_name => "User"
 
-	def self.change_definition_id_to_word
-		Exemple.all.each do |eg|
-			word = eg.definition.word
-			eg.update(definition_id: word.id)
-		end
-	end
+	belongs_to :contributor, :class_name => "User"
 
 end
--- db/migrate/20180416095402_exemple_belongs_to_word_not_definiton.rb
+++ db/migrate/20180416095402_exemple_belongs_to_word_not_definiton.rb
@@ -1,5 +1,5 @@
 class ExempleBelongsToWordNotDefiniton < ActiveRecord::Migration[5.0]
   def change
-    change_column :exemples, :definition_id, :word_id
+    rename_column :exemples, :definition_id, :word_id
   end
 end
