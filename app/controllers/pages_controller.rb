f301fcdafa40c084cb5c846b52cdd49ef7c08120
shcyiza <patick.cyiza@gmail.com>
2018-04-24 01:04:16 +0200

    add word_id and words to be deleted to the xlsx sheet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,16 @@
+# frozen_string_literal: true
 class PagesController < ApplicationController
-
-
-
   def home
     @search = Search.new
   end
 
-  def about
-  end
+  def about; end
 
-  def contact
-  end
+  def contact; end
 
-  def team
-  end
+  def team; end
 
-  def terms
-  end
+  def terms; end
 
   def admin
     @users = User.all
@@ -29,19 +23,18 @@ class PagesController < ApplicationController
   def admin_words
     @words = Word.all.order('name ASC').where.not(name: [nil, '']).page(params[:page])
     @all_words = Word.all.order('name ASC').where.not(name: [nil, ''])
-    @timestamp = "admin_export_#{Time.now.strftime("%d%m%Y%H%M%S")}"
+    @timestamp = "admin_export_#{Time.now.strftime('%d%m%Y%H%M%S')}"
     respond_to do |format|
       format.html
-      format.xlsx {
-        response.headers["Content-Disposition"] = "attachment; filename='#{@timestamp}.xlsx'"
-      }
+      format.xlsx do
+        response.headers['Content-Disposition'] = "attachment; filename='#{@timestamp}.xlsx'"
+      end
     end
   end
 
   def admin_translations
     @translations = DefinitionTranslation.all
   end
-
 
   def admin_definitions_themes
     @labelisations = DefinitionTheme.all
